bcc0a155e64e895be655df85b92cbb8f0f82015a
Armijosluis <luis.j.armijos@unl.edu.ec>
2024-07-07 15:00:20 -0500

    elementos impares.cpp

--- Matrices/elementos impares.cpp
+++ Matrices/elementos impares.cpp
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 int main() {
+	
+	   
+    printf("              **Elementos impares de la matriz**\n");
     int matriz[3][3];
-    
-    matriz[0][0] = 4;
-    matriz[0][1] = 5;
-    matriz[0][2] = 2;
-    matriz[1][0] = 4;
-    matriz[1][1] = 3;
-    matriz[1][2] = 2;
-    matriz[2][0] = 7;
-    matriz[2][1] = 1;
-    matriz[2][2] = 0;
+
+    // Pedir al usuario que ingrese los 9 valores de la matriz en una sola línea
+    printf("Ingrese los 9 valores de la matriz 3x3 separados por espacios:");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
 
     // Imprimir la matriz completa
-    printf("Matriz:\n");
+    printf("\nMatriz:\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             printf("%d ", matriz[i][j]);
@@ -22,22 +23,18 @@ int main() {
         printf("\n");
     }
 
-   // elementos impares de la matriz
+    // Imprimir los elementos impares de la matriz
     printf("\nElementos impares de la matriz:\n");
-   
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            if (matriz[i][j] %2 !=0 ){
-             printf(" %d ", matriz[i][j]);
+            if (matriz[i][j] % 2 != 0) {
+                printf("%d ", matriz[i][j]);
             } else {
-                printf("   "); 
-        
-         }
-    
-     }
-     printf("\n");
- }
-    
-    
+                printf("   "); // Espacio en blanco para los elementos pares
+            }
+        }
+        printf("\n");
+    }
+
     return 0;
-	}
+}
