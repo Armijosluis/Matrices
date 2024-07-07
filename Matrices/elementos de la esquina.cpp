6c77422b49451e09dbbb1c48916957e282987105
Armijosluis <luis.j.armijos@unl.edu.ec>
2024-07-07 14:54:40 -0500

    elementos de la esquina.cpp

--- Matrices/elementos de la esquina.cpp
+++ Matrices/elementos de la esquina.cpp
@@ -2,31 +2,29 @@
 
 int main() {
     int matriz[3][3];
-    
-    matriz[0][0] = 4;
-    matriz[0][1] = 5;
-    matriz[0][2] = 2;
-    matriz[1][0] = 4;
-    matriz[1][1] = 3;
-    matriz[1][2] = 2;
-    matriz[2][0] = 7;
-    matriz[2][1] = 1;
-    matriz[2][2] = 0;
+    int i, j;
+
+    // Pedir al usuario que ingrese los valores de la matriz en una sola línea
+    printf("Ingrese los 9 valores de la matriz 3x3:");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
 
     // Imprimir la matriz completa
-    printf("Matriz:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    printf("\nMatriz:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
 
-    // los 4 elementos de la esquina
-    printf("\nElementos de las esquinas :\n");
-    printf("%d    %d \n", matriz[0][0], matriz[0][2]);
-    printf("\n");
-    printf("%d    %d \n", matriz[2][0], matriz[2][2]);
-    
+    // Imprimir los 4 elementos de la esquina
+    printf("\nElementos de las esquinas:\n");
+    printf("%d    %d\n", matriz[0][0], matriz[0][2]);
+    printf("%d    %d\n", matriz[2][0], matriz[2][2]);
+
     return 0;
 	}
