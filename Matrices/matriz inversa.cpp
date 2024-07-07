db632e8fc330abc3beb0c35afe570cb743631e7a
Armijosluis <luis.j.armijos@unl.edu.ec>
2024-07-07 17:45:24 -0500

    matriz inversa.cpp

--- Matrices/matriz inversa.cpp
+++ Matrices/matriz inversa.cpp
@@ -3,21 +3,18 @@
 int main() {
     printf("Ingrese los datos de la matriz\n");
 
-    float matriz[3][3];//Variable de la matriz
-    float aux;//Auxiliar que usaremos mas adelante
-    float pivote;//Otro auxiliar 
-    float identidad[3][3];//Matriz identidad 
-    float determinante = 0;//Variable para el determinante 
+    float matriz[3][3];
+    float identidad[3][3];
+    float determinante = 0;
 
-//Ingresamos los datos de la matriz
+    // Ingresamos los datos de la matriz
     for (int i = 0; i < 3; i++) {
-
         for (int j = 0; j < 3; j++) {
             scanf("%f", &matriz[i][j]);
         }
     }
 
-//Mostramos los datos de la matriz ingresada
+    // Mostramos los datos de la matriz ingresada
     printf("Matriz ingresada\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -26,23 +23,22 @@ int main() {
         printf("\n");
     }
 
-//Calculamos el determinante
+    // Calculamos el determinante
     determinante = matriz[0][0] * (matriz[1][1] * matriz[2][2] - matriz[2][1] * matriz[1][2]) -
                    matriz[0][1] * (matriz[1][0] * matriz[2][2] - matriz[2][0] * matriz[1][2]) +
                    matriz[0][2] * (matriz[1][0] * matriz[2][1] - matriz[2][0] * matriz[1][1]);
 
-    if (determinante != 0) {//Conodicionamos de acuerdo al determinante
-//Iniciamos la matriz identidad
+    if (determinante != 0) {
+        // Iniciamos la matriz identidad
         for (int i = 0; i < 3; i++) {
-
             for (int j = 0; j < 3; j++) {
                 identidad[i][j] = (i == j) ? 1.0 : 0.0;
             }
         }
 
-//Proceso de Gauss-Jordan
+        // Proceso de Gauss-Jordan para calcular la inversa
+        float pivote, aux;
         for (int i = 0; i < 3; i++) {
-
             pivote = matriz[i][i];
             for (int k = 0; k < 3; k++) {
                 matriz[i][k] /= pivote;
@@ -59,7 +55,7 @@ int main() {
             }
         }
 
-//Mostramos la matriz inversa
+        // Mostramos la matriz inversa
         printf("Matriz inversa:\n");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
